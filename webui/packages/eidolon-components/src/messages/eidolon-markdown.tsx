90345d3d4dbab37720954e7462e8a6a28cbf5f27
Jacob Habib <jahgoreds@gmail.com>
2024-06-03 23:55:00 -0500

    fix markdown issues

--- webui/packages/eidolon-components/src/messages/eidolon-markdown.tsx
+++ webui/packages/eidolon-components/src/messages/eidolon-markdown.tsx
@@ -1,11 +1,6 @@
-// @ts-ignore
 import Markdown, {uriTransformer} from "react-markdown";
-// @ts-ignore
 import remarkGfm from "remark-gfm";
-// @ts-ignore
 import rehypeRaw from 'rehype-raw'
-// @ts-ignore
-import rehypeWrap from "rehype-wrap-all";
 import {Prism as SyntaxHighlighter} from "react-syntax-highlighter";
 import {materialLight} from "react-syntax-highlighter/dist/esm/styles/prism";
 import "./eidolon-markdown.css"
